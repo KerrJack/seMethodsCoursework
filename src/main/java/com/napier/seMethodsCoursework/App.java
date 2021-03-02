a80078022437cf162906e1c5f3c8f97cfbbcc575
Rory <40439757@live.napier.ac.uk>
2021-03-02 23:58:58 +0000

    spelling errors amended

--- src/main/java/com/napier/seMethodsCoursework/App.java
+++ src/main/java/com/napier/seMethodsCoursework/App.java
@@ -112,7 +112,7 @@ public class App {
         catch (Exception e)
         {
             System.out.println(e.getMessage());
-            System.out.println("Failed to get salary details");
+            System.out.println("Failed to get population details");
             return null;
         }
     }
