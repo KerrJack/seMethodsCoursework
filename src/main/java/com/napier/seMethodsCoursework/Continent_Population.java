500ff9b5e6c8bc5f2a541f8c68625751636d6075
Rory <40439757@live.napier.ac.uk>
2021-04-29 13:06:57 +0100

    report  22 complete, new class for report added

---
+++ src/main/java/com/napier/seMethodsCoursework/Continent_Population.java
@@ -0,0 +1,84 @@
+package com.napier.seMethodsCoursework;
+
+public class Continent_Population {
+
+
+    //initialise getter and setter for Continent
+    private String countryContinent_continent;
+
+    public String getCountryContinent_continent() {
+        return countryContinent_continent;
+    }
+
+    public void setCountryContinent_continent(String countryContinent_continent) {
+        this.countryContinent_continent = countryContinent_continent;
+    }
+
+
+    //initialise getter and setter for Continent population
+    private Long totalPopulation_continent;
+
+    public Long getTotalPopulation_continent() {
+        return totalPopulation_continent;
+    }
+
+    public void setTotalPopulation_continent(Long totalPopulation_continent) {
+        this.totalPopulation_continent = totalPopulation_continent;
+    }
+
+
+    //initialise getter and setter for city Continent population
+    private Long totalCityPopulation_continent;
+
+    public Long getTotalCityPopulation_continent() {
+        return totalCityPopulation_continent;
+    }
+
+    public void setTotalCityPopulation_continent(Long totalCityPopulation_continent) {
+        this.totalCityPopulation_continent = totalCityPopulation_continent;
+    }
+
+    //initialise getter and setter for city Continent population percentage
+    private Long cityPopulationPercentage_continent;
+
+    public Long getCityPopulationPercentage_continent() {
+        return cityPopulationPercentage_continent;
+    }
+
+    public void setCityPopulationPercentage_continent(Long cityPopulationPercentage_continent) {
+        this.cityPopulationPercentage_continent = cityPopulationPercentage_continent;
+    }
+
+
+
+    //initialise getter and setter for not in city Continent population
+    private Long notInCityPopulation_continent;
+
+    public Long getNotInCityPopulation_continent() {
+        return notInCityPopulation_continent;
+    }
+
+    public void setNotInCityPopulation_continent(Long notInCityPopulation_continent) {
+        this.notInCityPopulation_continent = notInCityPopulation_continent;
+    }
+
+
+    //initialise getter and setter for not in city Continent population percentage
+    private Long notInCityPopulationPercentage_continent;
+
+    public Long getNotInCityPopulationPercentage_continent() {
+        return notInCityPopulationPercentage_continent;
+    }
+
+    public void setNotInCityPopulationPercentage_continent(Long notInCityPopulationPercentage_continent) {
+        this.notInCityPopulationPercentage_continent = notInCityPopulationPercentage_continent;
+    }
+
+
+
+
+
+
+
+
+}
