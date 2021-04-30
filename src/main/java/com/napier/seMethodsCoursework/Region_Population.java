8bb86cb9f87ba1bb45402a7f3d48f379af6207f9
Rory <40439757@live.napier.ac.uk>
2021-04-30 14:10:31 +0100

    report  23 complete, new class for report added

---
+++ src/main/java/com/napier/seMethodsCoursework/Region_Population.java
@@ -0,0 +1,75 @@
+package com.napier.seMethodsCoursework;
+
+public class Region_Population {
+    //initialise getter and setter for Continent
+    private String countryRegion_Region;
+
+    public String getCountryRegion_Region() {
+        return countryRegion_Region;
+    }
+
+    public void setCountryRegion_Region(String countryRegion_Region) {
+        this.countryRegion_Region = countryRegion_Region;
+    }
+
+
+    //initialise getter and setter for Continent population
+    private Long totalPopulation_Region;
+
+    public Long getTotalPopulation_Region() {
+        return totalPopulation_Region;
+    }
+
+    public void setTotalPopulation_Region(Long totalPopulation_Region) {
+        this.totalPopulation_Region = totalPopulation_Region;
+    }
+
+
+    //initialise getter and setter for city Continent population
+    private Long totalCityPopulation_Region;
+
+    public Long getTotalCityPopulation_Region() {
+        return totalCityPopulation_Region;
+    }
+
+    public void setTotalCityPopulation_Region(Long totalCityPopulation_Region) {
+        this.totalCityPopulation_Region = totalCityPopulation_Region;
+    }
+
+    //initialise getter and setter for city Continent population percentage
+    private Long cityPopulationPercentage_Region;
+
+    public Long getCityPopulationPercentage_Region() {
+        return cityPopulationPercentage_Region;
+    }
+
+    public void setCityPopulationPercentage_Region(Long cityPopulationPercentage_Region) {
+        this.cityPopulationPercentage_Region = cityPopulationPercentage_Region;
+    }
+
+
+
+    //initialise getter and setter for not in city Continent population
+    private Long notInCityPopulation_Region;
+
+    public Long getNotInCityPopulation_Region() {
+        return notInCityPopulation_Region;
+    }
+
+    public void setNotInCityPopulation_Region(Long notInCityPopulation_Region) {
+        this.notInCityPopulation_Region = notInCityPopulation_Region;
+    }
+
+
+    //initialise getter and setter for not in city Continent population percentage
+    private Long notInCityPopulationPercentage_Region;
+
+    public Long getNotInCityPopulationPercentage_Region() {
+        return notInCityPopulationPercentage_Region;
+    }
+
+    public void setNotInCityPopulationPercentage_Region(Long notInCityPopulationPercentage_Region) {
+        this.notInCityPopulationPercentage_Region = notInCityPopulationPercentage_Region;
+    }
+
+}
